e98244c170380b5f9898dc2a52b9dd482392af68
tushar.kasturi_ymedi <tusharkasturi1999@gmail.com>
2021-09-08 09:19:01 +0530

    Added: defineCards method to define cards

--- main/java/com/bridgelabz/card/Card.java
+++ main/java/com/bridgelabz/card/Card.java
@@ -1,25 +1,33 @@
 package com.bridgelabz.card;
 
 /**
- * @author tushar.kasturi_ymedi
- * This class consist of cards class
+ * @author tushar.kasturi_ymedi This class consist of cards class
+ * @param suits defines the suits
+ * @param ranks defines the ranks
+ * @param deck  its a integer array of deck
+ * @param cardCount keeps the count of cards in the deck
  */
 public class Card {
 
-	/**
-	 * This method deines the deck of cards
-	 * @param suits defines the suits
-	 * @param ranks defines the ranks
-	 * @param deck its a integer array of deck
-	 */
-	public void defineCards(){
-	int[] deck = new int[52];
-	String[] suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
-	String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+	private final String[] deck = new String[52];
+	private final String[] suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
+	private final String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+	private static int cardCount = 0;
+
+	//This method assigns card to the deck
+	public void initialize() {
+		for (int sIndex = 0; sIndex < ranks.length; sIndex++) {
+			for (int rIndex = 0; rIndex < suits.length; rIndex++) {
+				deck[cardCount] = ranks[sIndex] + " of " + suits[rIndex];
+				cardCount++;
+			}
+		}
 	}
 
-	//This is the main class
+	// This is the main class
 	public static void main(String[] args) {
 		System.out.println("Welcome to Card Game");
+		Card deck = new Card();
+		deck.initialize();
 	}
 }
