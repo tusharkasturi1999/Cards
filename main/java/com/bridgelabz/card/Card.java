df53251fbaf0f5482203ba46330e9420c962de4e
tushar.kasturi_ymedi <tusharkasturi1999@gmail.com>
2021-09-08 08:50:32 +0530

    Added: defineCards method to define cards

--- main/java/com/bridgelabz/card/Card.java
+++ main/java/com/bridgelabz/card/Card.java
@@ -1,8 +1,25 @@
 package com.bridgelabz.card;
 
+/**
+ * @author tushar.kasturi_ymedi
+ * This class consist of cards class
+ */
 public class Card {
 
-	public static void main( String[] args ) {
+	/**
+	 * This method deines the deck of cards
+	 * @param suits defines the suits
+	 * @param ranks defines the ranks
+	 * @param deck its a integer array of deck
+	 */
+	public void defineCards(){
+	int[] deck = new int[52];
+	String[] suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
+	String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+	}
+
+	//This is the main class
+	public static void main(String[] args) {
 		System.out.println("Welcome to Card Game");
 	}
 }
