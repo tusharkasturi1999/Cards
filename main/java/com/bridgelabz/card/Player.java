15ab2234bfa874e26eab66b55710297c82c58ccb
tushar.kasturi_ymedi <tusharkasturi1999@gmail.com>
2021-09-14 17:25:36 +0530

    Added: Player class

--- main/java/com/bridgelabz/card/Player.java
+++ main/java/com/bridgelabz/card/Player.java
@@ -1,30 +1,32 @@
 package com.bridgelabz.card;
 
 import java.util.*;
+
 /**
  * class to store the number of players and their cards
- * @author Adithya S Shenoy
- *
+ * @author tushar.kasturi_ymedi
  */
-class individualPlayer{
-   /**
-    * individualPlayer is each player with 9 cards
-    */
-    public individualPlayer()
-    {
-        ArrayList<Card> individualPlayer=new ArrayList<Card>(9); 
-    } 
+class individualPlayer {
+	/**
+	 * individualPlayer is each player with 9 cards
+	 */
+	public individualPlayer() {
+		ArrayList<Card> individualPlayer = new ArrayList<Card>(9);
+	}
 }
 
 public class Player {
-    /**
-     * constructor to initialize the players
-     * @param n number of players
-     */
-    public Player(int n)
-    {
-        Deck cards=new Deck();
-        //players contain 9 cards of individual player
-        ArrayList<individualPlayer> players = new ArrayList<individualPlayer>(n);    
-    }
+	/**
+	 * constructor to initialize the players
+	 * 
+	 * @param n number of players
+	 */
+	private static int noOfPlayers;
+
+	public Player(int noOfPlayers) {
+		Deck cards = new Deck();
+		// players contain 9 cards of individual player
+		ArrayList<individualPlayer> players = new ArrayList<individualPlayer>(noOfPlayers);
+	}
+
 }
